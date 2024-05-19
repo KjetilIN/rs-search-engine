1fd92e9cba4d9085fa22deed66d01702db7bf3f9
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-05-19 17:07:43 +0200

    add: parsing the html tags

--- src/main.rs
+++ src/main.rs
@@ -7,9 +7,11 @@ const FOLDER_PATH: &str = "./pages/";
 
 
 fn parse_file_html(file_path: &str) -> Result<TokenizedDocument, Error>{
-    let mut map: HashMap<_, _> = HashMap::new();
+    // Create new map for the tokens
+    let mut map: TokenizedDocument = HashMap::new();
 
-    let mut file =  match File::open(file_path){
+    // Read the file 
+    let file =  match File::open(file_path){
         Ok(file) => file,
         Err(e) => {
             eprintln!("[ERROR] Was not able to find file {}: {}", file_path, e);
@@ -17,17 +19,32 @@ fn parse_file_html(file_path: &str) -> Result<TokenizedDocument, Error>{
         }
     };
 
+    // Read content to buffer reader 
     let mut content = String::new();
     let mut buf_reader = BufReader::new(file);
-
-    // Read the content to the mutable variable content
     buf_reader.read_to_string(&mut content).unwrap_or_else(|err| {
         eprintln!("[ERROR] could not read file {}: {}", file_path, err);
         panic!()
     });
 
-    for line in content.lines(){
-        println!("{}", line);
+    // Iterate over each file 
+    for mut line in content.lines(){
+        let mut open_tag_index: i32 = -1; 
+        for (index, char) in line.chars().enumerate(){
+            if char == '<'{
+                open_tag_index = index as i32;
+                continue;
+            }else if char == '>' {
+                if open_tag_index > -1 {
+                    // Tag can be removed
+                    line = line[..open_tag_index] + line[open_tag_index..];
+                }else{
+                    eprintln!("[ERROR] Found '>' without a opening tag in '{line}': {file_path}");
+                    exit(1)
+                }
+                
+            }
+        }
     }
 
     Ok(map)
@@ -38,14 +55,18 @@ fn parse_file_md(file_path: &str) -> Result<Option<TokenizedDocument>, Error>{
     unimplemented!("Parse Markdown File")
 }
 
-fn parse_folder(folder_path: &str) -> Result<FolderTokens, ()> {
+fn parse_dir(folder_path: &str) -> Result<FolderTokens, ()> {
     let mut folder_tokens = HashMap::new();
 
-    let paths = fs::read_dir(folder_path).unwrap();
+    let paths = fs::read_dir(folder_path).unwrap_or_else(|err| {
+        eprintln!("[ERROR] Was not able to read dir path {folder_path}:{err}");
+        exit(1)
+    });
 
     for path in paths{
         if let Some(current_path) = path.unwrap().path().to_str(){
             println!("[INFO] Parsing file {}", current_path);
+            _ = parse_file_html(current_path);
         }else{
             eprintln!("[ERROR] File not available");
             exit(1)
@@ -57,6 +78,6 @@ fn parse_folder(folder_path: &str) -> Result<FolderTokens, ()> {
 
 fn main() {
     
-    _ = parse_folder(FOLDER_PATH);
+    _ = parse_dir(FOLDER_PATH);
 
 }
