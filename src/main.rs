55ebda490146bf4955c29a3b91a6c7811ffaa5cc
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-05-19 17:15:50 +0200

    add: handling opening tag error

--- src/main.rs
+++ src/main.rs
@@ -28,16 +28,20 @@ fn parse_file_html(file_path: &str) -> Result<TokenizedDocument, Error>{
     });
 
     // Iterate over each file 
-    for mut line in content.lines(){
+    for line in content.lines(){
+        let mut line_to_tokenize: String = line.to_string();
         let mut open_tag_index: i32 = -1; 
+
+        // Iterate over each char in the line and 
         for (index, char) in line.chars().enumerate(){
             if char == '<'{
                 open_tag_index = index as i32;
                 continue;
             }else if char == '>' {
                 if open_tag_index > -1 {
-                    // Tag can be removed
-                    line = line[..open_tag_index] + line[open_tag_index..];
+                    // Tag can be removed, and reset start index
+                    line_to_tokenize.replace_range(open_tag_index as usize..index, "");
+                    open_tag_index = -1; 
                 }else{
                     eprintln!("[ERROR] Found '>' without a opening tag in '{line}': {file_path}");
                     exit(1)
@@ -45,6 +49,15 @@ fn parse_file_html(file_path: &str) -> Result<TokenizedDocument, Error>{
                 
             }
         }
+
+        // Check if there is an opening tag without closing tag
+        if open_tag_index != -1{
+            eprintln!("[ERROR] Found '<' without a closing tag in {file_path}");
+            exit(1)
+        }
+
+        // Collect tokens 
+        let tokens: Vec<&str> = line_to_tokenize.split(" ").into_iter().collect();   
     }
 
     Ok(map)
