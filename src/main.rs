8666fca2f0cfe4e5422d04c7e68a864256343664
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-05-23 18:44:57 +0200

    add: basic rust http server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::process::exit;
+
+use tiny_http::Server;
 use types::FolderTokens;
 
 mod parse;
@@ -5,9 +8,30 @@ pub mod types;
 pub mod tf;
 
 fn main() {
-    let folder_path: &str = "./pages/";
-    let documents: FolderTokens = parse::parse_dir(folder_path, true, true).unwrap();
 
-    println!("FOLDER: {:?}", documents);
+    let addr = "127.0.0.1:8080";
+    let server = Server::http(addr).unwrap_or_else(|err| {
+        eprintln!("[ERROR] Could not start HTTP server on {addr}: {err}");
+        exit(1)
+    });
+
+    println!("[INFO] Serving a HTTP server on {addr}");
+
+    loop {
+        // Read request from server
+        let request = match server.recv(){
+            Ok(req) => req,
+            Err(err) => {
+                eprintln!("[ERROR] Could not handle request: {err}");
+                continue;
+            } 
+        }; 
+
+        println!("[INFO] URL {}", request.url());    
+    }
+
+    //let folder_path: &str = "./pages/";
+    //let documents: FolderTokens = parse::parse_dir(folder_path, true, true).unwrap();
+    //println!("FOLDER: {:?}", documents);
 
 }
