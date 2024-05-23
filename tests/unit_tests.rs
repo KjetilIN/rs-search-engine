09782032d3ad2543db34e36459a48182e6007ad5
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-05-23 18:09:24 +0200

    add: unit test for checking correct setup of kubernetes setup

--- tests/unit_tests.rs
+++ tests/unit_tests.rs
@@ -1,7 +1,26 @@
 #[cfg(test)]
 mod tests {
+    use walkdir::WalkDir;
+
     #[test]
-    fn test_setup(){
-        assert_eq!(1,1);
+    fn test_pages_setup(){
+        let pages_path = "./pages";
+        let mut counter: usize = 0; 
+
+        let walker = WalkDir::new(pages_path).into_iter();
+        
+        for entry in walker{
+            match entry {
+                Ok(entry) => {
+                    let path = entry.file_name().to_string_lossy();
+                    if path.ends_with(".html"){
+                        counter += 1;
+                    }
+                },
+                _ => (),
+            }
+        }
+
+        assert_eq!(78, counter, "[ERROR]Test that there are 78 html files in the setup page, was {counter}");
     }
 }
