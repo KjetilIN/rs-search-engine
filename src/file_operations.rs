d8b8768ce2efe4e3f5c0cb9b4953cf77de5115d1
Kjetil Indrehus <kjetilindrehus@gmail.com>
2024-05-24 12:46:32 +0200

    add: file save and load tokenized text with serde and bincode

--- src/file_operations.rs
+++ src/file_operations.rs
@@ -1,5 +1,9 @@
 use std::{error::Error, fs::File, io::{BufReader, Read}};
 
+use crate::types::FolderTokens;
+
+const CACHE_FILE_PATH: &str = "./cache/hashmap_cache.dat";
+
 pub fn read_file(file_path: &str) -> Result<String, Box<dyn Error>>{
     // Read the file 
     let file =  match File::open(file_path){
@@ -20,4 +24,42 @@ pub fn read_file(file_path: &str) -> Result<String, Box<dyn Error>>{
 
     // Ok
     Ok(content)
+}
+
+pub fn save_to_file(folder:FolderTokens) -> Result<(), Box<dyn Error>>{
+    let file = match File::create(CACHE_FILE_PATH){
+        Ok(f) => f,
+        Err(err) => {
+            eprintln!("[ERROR] Could not create file: {err}");
+            return Err(Box::new(err));
+        },
+    };
+
+    match bincode::serialize_into(file, &folder){
+        Ok(_) => return Ok(()),
+        Err(err) => {
+            eprintln!("[ERROR] Could write to file: {err}");
+            return Err(Box::new(err));
+        },
+    };
+}
+
+pub fn load_from_file() -> Result<FolderTokens, Box<dyn Error>>{
+    let file = match File::open(CACHE_FILE_PATH){
+        Ok(f) => f,
+        Err(err) => {
+            eprintln!("[ERROR] Could not create file: {err}");
+            return Err(Box::new(err));
+        },
+    };
+
+    let folder: FolderTokens = match bincode::deserialize_from(file) {
+        Ok(f) => f,
+        Err(err) => {
+            eprintln!("[ERROR] Could not read from file: {err}");
+            return Err(Box::new(err));
+        },
+    };
+
+    Ok(folder)
 }
